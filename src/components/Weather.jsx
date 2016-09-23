3ff334212b9a13cbc9acef89c4f7e6540afd7929
Gustav Jorlöv <gustav.jorlov@kits.se>
2016-09-24 00:18:46 +0200

    temperature showing

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -5,11 +5,32 @@ export default class Weather extends React.Component{
     constructor(){
         super();
         console.log("constructor");
+        this.state = {
+            today: {
+                temperature: "-",
+                summary: "-",
+                precipitation: "-",
+                icon: "-"
+            }
+        };
+    }
+    getTodaysWeather(responseBody){
+        const weatherData = JSON.parse(responseBody);
+        const temperature = Math.round(10*(weatherData.currently.temperature-32)*5/9)/10;
+        const summary = weatherData.currently.summary;
+        const precipitation = weatherData.currently.precipIntensity;
+        const icon = weatherData.currently.icon;
+        return {today: {
+            temperature: temperature,
+            summary: summary,
+            precipitation: precipitation,
+            icon: icon
+        }};
     }
     componentDidMount(){
         $.ajax("/weather", {
-            success: function(response, status){
-                console.log(response);
+            success: (response, status) => {
+                this.setState(this.getTodaysWeather(response));
             },
             error: function(){
                 console.log(":()");
@@ -17,6 +38,9 @@ export default class Weather extends React.Component{
         });
     }
     render(){
-        return (<h2>Weather!</h2>);
+        return (<div>
+            <h2>{this.state.today.summary}</h2>
+            <h3>{this.state.today.temperature} &ordm;C</h3>
+        </div>);
     }
 }
